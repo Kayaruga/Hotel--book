def22a68d46a11fe0f3ac8dd109860270e543ab9
Thxknqz <kayaruga77@gmail.com>
2026-07-27 22:31:19 +0700

    Java_script update

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const yedtod
+function filterRooms(type, btn) {
+    document.querySelectorAll('.filter-btn').forEach(b => {
+        b.classList.remove('active');
+    });
+
+    btn.classList.add('active');
+
+    document.querySelectorAll('.grid-item').forEach(card => {
+        const status = card.dataset.status;
+        const roomtype = card.dataset.type;
+
+        if (type === 'all') {
+            card.classList.remove('hidden')
+        } else if (type === 'available') {
+            card.classList.toggle('hidden', status !== 'available');
+        } else {
+            card.classList.toggle('hidden', roomtype !== type)
+        }
+    })
+}
+
+
